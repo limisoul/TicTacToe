3e5793b2798b5bad6c0de59274f2f8e8944c78f6
limisoul <f332116469@gmail.com>
2024-04-02 22:50:24 +0800

    新增icon图标和部分css样式设定

---
+++ src/js/icon.js
@@ -0,0 +1,16 @@
+const Unicode = {
+  logo: `&#x0F003B`,
+
+  // 箭头
+  arrow_down:   `&#x0F0045`,
+  arrow_up:     `&#x0F005D`,
+  arrow_left:   `&#x0F004D`,
+  arrow_right:  `&#x0F0054`,
+  arrow_ld:     `&#x0F0042`,
+  arrow_rd:     `&#x0F0043`,
+  arrow_ru:     `&#x0F005C`,
+  arrow_lu:     `&#x0F005B`,
+
+  audio_play:   `&#xFA7D`,
+  audio_mute:   `&#xFA80`,
+}
